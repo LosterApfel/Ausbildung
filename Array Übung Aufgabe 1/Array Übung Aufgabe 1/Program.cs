c9fdc84a384f8372ea12d29fcd24e717ab114077
Luca Eßlinger <luca.esslinger@mail.schwarz>
2021-11-15 08:32:55 +0100

    Array Aufgabe

---
+++ Array Übung Aufgabe 1/Array Übung Aufgabe 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+namespace Array_Übung_Aufgabe_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList Namensliste = new ArrayList();
+            Namensliste.Add("Michi");
+            Namensliste.Add("Sissi");
+            Namensliste.Add("Denis");
+            Namensliste.Add("Franz");
+            Namensliste.Add("Nummer5");
+            Console.WriteLine("Nach wem suchst du?");
+            string eingabe = Console.ReadLine();
+            
+
+        }
+    }
+}
