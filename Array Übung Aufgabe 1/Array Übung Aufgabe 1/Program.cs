0902f0a167267fbaafe6ad96898c39ef6793ada1
Luca Eßlinger <luca.esslinger@mail.schwarz>
2021-11-15 09:56:10 +0100

    Verbessert

--- Array Übung Aufgabe 1/Array Übung Aufgabe 1/Program.cs
+++ Array Übung Aufgabe 1/Array Übung Aufgabe 1/Program.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections;
-namespace Array_Übung_Aufgabe_1
+
+namespace Aufgabe2
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             ArrayList Namensliste = new ArrayList();
+
             Namensliste.Add("Michi");
             Namensliste.Add("Sissi");
             Namensliste.Add("Denis");
             Namensliste.Add("Franz");
             Namensliste.Add("Nummer5");
-            Console.WriteLine("Nach wem suchst du?");
-            string eingabe = Console.ReadLine();
             
+            Console.WriteLine("Nach wem suchst du?");
 
+            string eingabe = Console.ReadLine();
+            if (Namensliste.Contains(eingabe))
+            {
+                Console.WriteLine(eingabe + " ist der Schulklasse vorhanden");
+            }
+            else
+            {
+                Console.WriteLine(eingabe + " ist nicht vorhanden");
+            }
+            Console.ReadKey();
         }
     }
 }
